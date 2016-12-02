82f2a1adbcbcabf90050efd192f33e2e0a03814b
HsunFu Liu <Luckman@Lius-MacBook-Pro.local>
2016-12-01 23:51:40 -0800

    1.Move the node_modules to Ani/node_modules, so remember to cd to Ani/ folder if u want to start the js.  2.Move the MongoDB dataPath to Ani/272_Team6_ProjectRabbitMqServer/data. Remember to use --dbpath to point out your database path of mongoDB to this folder.   3.Modify the luckman.sh. If you use osx, just use this bash file to start all servers.

--- SmartIDE/luckman.sh
+++ SmartIDE/luckman.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/project/Fall16-Team6/SmartIDE; python controller/controller.py"'
 osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/project/Fall16-Team6/SmartIDE; python GoogleAPI_DatParser/GoogleAPI/googleapi.py"'
-osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/project/Fall16-Team6/SmartIDE; mongod --dbpath Ani/272_Team6_Project/data --port 27017"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/project/Fall16-Team6/SmartIDE; mongod --dbpath Ani/272_Team6_ProjectRabbitMqServer/data --port 27017"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/project/Fall16-Team6/SmartIDE; /usr/local/sbin/rabbitmq-server start"'
 osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/project/Fall16-Team6/SmartIDE; node Ani/272_Team6_Project/bin/www"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/project/Fall16-Team6/SmartIDE; node Ani/272_Team6_ProjectRabbitMqServer/server.js"'
+
+
 
