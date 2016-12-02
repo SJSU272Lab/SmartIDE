21b6e94a68fe5c7d9d6ba3d96373d03a97dbb7ff
HsunFu Liu <Luckman@Lius-MacBook-Pro.local>
2016-12-02 02:36:22 -0800

    Change the bash file luckman.sh

--- SmartIDE/luckman.sh
+++ SmartIDE/luckman.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/project/Fall16-Team6/SmartIDE; python controller/controller.py"'
-osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/project/Fall16-Team6/SmartIDE; python GoogleAPI_DatParser/GoogleAPI/googleapi.py"'
-osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/project/Fall16-Team6/SmartIDE; mongod --dbpath Ani/272_Team6_ProjectRabbitMqServer/data --port 27017"'
-osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/project/Fall16-Team6/SmartIDE; /usr/local/sbin/rabbitmq-server start"'
-osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/project/Fall16-Team6/SmartIDE; node Ani/272_Team6_Project/bin/www"'
-osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/project/Fall16-Team6/SmartIDE; node Ani/272_Team6_ProjectRabbitMqServer/server.js"'
+# run controller
+osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/Final_Project/Fall16-Team6/SmartIDE; python controller/controller.py"'
+# run googleapi
+osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/Final_Project/Fall16-Team6/SmartIDE; python GoogleAPI_DatParser/GoogleAPI/googleapi.py"'
+# run monogoDB with dbpath Ani/272_Team6_ProjectRabbitMqServer/data
+osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/Final_Project/Fall16-Team6/SmartIDE; mongod --dbpath Ani/272_Team6_ProjectRabbitMqServer/data --port 27017"'
+# run rabbit
+osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/Final_Project/Fall16-Team6/SmartIDE; /usr/local/sbin/rabbitmq-server start"'
+# run restful service
+osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/Final_Project/Fall16-Team6/SmartIDE; node Ani/272_Team6_Project/bin/www"'
+# run message queue service
+osascript -e 'tell application "Terminal" to do script "cd /Users/Luckman/Documents/SJSU/Course/2016_Fall/272/Final_Project/Fall16-Team6/SmartIDE; node Ani/272_Team6_ProjectRabbitMqServer/server.js"'
 
 
 
