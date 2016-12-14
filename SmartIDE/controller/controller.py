e31705f807f9d2be22a6b6da08a3929223d2dabc
HsunFu Liu <Luckman@Lius-MacBook-Pro.local>
2016-12-13 19:31:41 -0800

    Exception all the connection error

--- SmartIDE/controller/controller.py
+++ SmartIDE/controller/controller.py
@@ -8,22 +8,65 @@ app = Flask(__name__)
 
 ## need to have mechanism to remove the duplicate between   DB   and   GOOGLE
 def mergeDict(dictA, dictB, dictC):
-    dictC_result = [{ "answer":dictC["QA"]["Answer"], "question":dictC["QA"]["Question"] }]
-    dictB_result = dictB
-    dictA_result = dictA["result"]
+    # dictA --> Google result
+    # dictB --> DB result
+    # dictC --> QA result
+    if(len(dictC)>0):
+        dictC_result = [{ "answer":dictC["QA"]["Answer"], "question":dictC["QA"]["Question"] }]
+    else:
+        dictC_result = {}
 
-    print dictB_result[0]
+    if(len(dictB)>0):
+        dictB_result = dictB
+    else:
+        dictB_result = {}
+
+    if(len(dictA)>0):
+        dictA_result = dictA["result"]
+    else:
+        dictA_result = {}
+
+    #print dictB_result[0]
+
+
+    check_dict = {}
+
+    if(len(dictB_result)>0):
+        for i in range(0, len(dictB_result)):
+            check_dict[dictB_result[i]["question"]] = 0
+
+    ## order:   dictB_result(DB) --> dictC_result(QA) --> dictA_result(Google)
 
     merged_result = []
-    merged_result.extend(dictC_result)
-    merged_result.extend(dictB_result)
-    merged_result.extend(dictA_result)
 
-    merged_dict = {
-        "keywords":dictA["keywords"],
-        "question":dictA["question"],
-        "result": merged_result
-    }
+    for i in range(0, len(dictC_result)):
+        if dictC_result[i]["question"] not in check_dict:
+            merged_result.append(dictC_result[i])
+    #merged_result.extend(dictC_result)
+    
+    merged_result.extend(dictB_result)
+
+
+
+    for i in range(0, len(dictA_result)):
+        if dictA_result[i]["question"] not in check_dict:
+            merged_result.append(dictA_result[i])
+
+    #merged_result.extend(dictA_result)
+    if(len(merged_result) >= 20):
+        length = 20
+    else:
+        length = len(merged_result)+1
+
+    if(len(dictA_result)>0):
+        merged_dict = {
+            "keywords":dictA["keywords"],
+            "question":dictA["question"],
+            "result": merged_result[0:length]
+        }
+    else:
+        print "Connection Error on [GOOGLE SEARCH API]"
+        merged_dict = {}
     return merged_dict
 
 
@@ -36,9 +79,16 @@ def api_GET_PUT_DELETE(keyword):
 
 
         #First, search the google 
-        r_google = requests.get('http://localhost:4000/search?question=%s'%keyword)
-        dictA = json.loads(r_google.text)
-        dictA["question"] =  keyword.replace("---", " ")
+        try:
+            r_google = requests.get('http://localhost:4000/search?question=%s'%keyword)
+            dictA = json.loads(r_google.text)
+            dictA["question"] =  keyword.replace("---", " ")
+        except:
+            print "Connection Error on [GOOGLE SEARCH API]"
+            js = { "Error": "Connection Error on [DataBase]"}
+            resp = Response(js, status=404, mimetype='application/json')
+            return resp
+
         #print dictA
         
         #Then, check the database by using the keyword google gave
@@ -51,42 +101,41 @@ def api_GET_PUT_DELETE(keyword):
         msg_payload_dic = {"keyword":keyword_str}
         msg_payload = json.dumps(msg_payload_dic)
         #print msg_payload
-        r_db = requests.post('http://localhost:3000/retrieveRecord', data=msg_payload_dic)
-        #print r_db.text
-        dictB = json.loads(r_db.text)
-        #print dictB
+        
 
-        dictB_extract = []
-        for i in range(0,len(dictB["message"])):
-            dictB_extract.append({ 
-                "id": dictB["message"][i]["_id"],
-                "answer": dictB["message"][i]["answer"],
-                "link": dictB["message"][i]["link"],
-                "question": dictB["message"][i]["question"],
-                "vote": dictB["message"][i]["votes"]
-            })
+        try:
+            r_db = requests.post('http://localhost:3000/retrieveRecord', data=msg_payload_dic)
+            #print r_db.text
+            dictB = json.loads(r_db.text)
+            #print dictB
+            dictB_extract = []
+            for i in range(0,len(dictB["message"])):
+                dictB_extract.append({ 
+                    "id": dictB["message"][i]["_id"],
+                    "answer": dictB["message"][i]["answer"],
+                    "link": dictB["message"][i]["link"],
+                    "question": dictB["message"][i]["question"],
+                    "vote": dictB["message"][i]["votes"]
+                })
+        except:
+            print "Connection Error on [DataBase]"
+            dictB_extract = []
         #print dictB_extract
 
 
         #Then, search the QA system
-        r_QA = requests.get('http://localhost:2666/qa/%s'%keyword)
-        dictC = json.loads(r_QA.text)
-        #print dictC
+        try:
+            r_QA = requests.get('http://localhost:2666/qa/%s'%keyword).text
+            dictC = json.loads(r_QA)
+        except:
+            print "Connection Error on [QA BOT]"
+            dictC = {}
+
         
-
-
-
-
-        ## need to have mechanism to remove the duplicate between   DB   and   GOOGLE
-        ## need to have mechanism to remove the duplicate between   DB   and   GOOGLE
-        ## need to have mechanism to remove the duplicate between   DB   and   GOOGLE
         
-        #merged_dict = {key: value for (key, value) in (dictA.items() + dictB.items() + dictC.items() )}
+        
         merged_dict_test = mergeDict(dictA, dictB_extract, dictC)
-        #print merged_dict_test
-
-        # string dump of the merged dict
-        #r = json.dumps(merged_dict)
+ 
         r = json.dumps(merged_dict_test)
 
         js = r
@@ -123,23 +172,28 @@ def api_POST():
             abort(404)
 
         resp_dict = json.loads(request.data)
-        print resp_dict
+        #print resp_dict
 
         # ## unmarshal the post request
         
         # Answer is from db ---> UPDATE
         if "id" in resp_dict["result"][0]:
-            print "!!!!!!!!!!!!!!! Answer is from db"
+            print "Answer is from db"
             msg_payload_dic = {
-                "id":resp_dict["result"][0],
+                "id":resp_dict["result"][0]["id"],
                 "vote": resp_dict["result"][0]["vote"]
             }
+            #print msg_payload_dic
             #insert it into DB
-            r_db = requests.post('http://localhost:3000/updateRecord', data=msg_payload_dic)
+            try:
+                r_db = requests.post('http://localhost:3000/updateRecord', data=msg_payload_dic)
+            except:
+                print "Connection Error on [DataBase]"
+                resp_dict = { "Error": "Connection Error on [DataBase]"}
 
         # Answer is from google ---> INSERT
         elif "link" in resp_dict["result"][0]:
-            print "!!!!!!!!!!!!!!! Answer is from google"
+            print "Answer is from google"
             msg_payload_dic = {
                 "keyword": resp_dict["keywords"],
                 "question": resp_dict["result"][0]["question"], 
@@ -148,11 +202,15 @@ def api_POST():
                 "link": resp_dict["result"][0]["link"]         
             }
             #insert it into DB
-            r_db = requests.post('http://localhost:3000/insertRecord', data=msg_payload_dic)
+            try:
+                r_db = requests.post('http://localhost:3000/insertRecord', data=msg_payload_dic)
+            except:
+                print "Connection Error on [DataBase]"
+                resp_dict = { "Error": "Connection Error on [DataBase]"}
 
         # Answer is from google ---> INSERT
         elif "question" in resp_dict["result"][0]:
-            print "!!!!!!!!!!!!!!! Answer is from QA"
+            print "Answer is from QA"
             msg_payload_dic = {
                 "keyword": resp_dict["keywords"],
                 "question": resp_dict["result"][0]["question"], 
@@ -160,22 +218,13 @@ def api_POST():
                 "vote": resp_dict["result"][0]["vote"],    
                 "link": ""    
             }
-            print msg_payload_dic
+            #print msg_payload_dic
             #insert it into DB
-            r_db = requests.post('http://localhost:3000/insertRecord', data=msg_payload_dic)
-
-        print r_db.text
-        # msg_payload_dic = {
-        #     "keyword": resp_dict["keywords"],
-        #     "question": resp_dict["result"][0]["question"], 
-        #     "answer": resp_dict["result"][0]["answer"], 
-        #     "votes": resp_dict["result"][0]["vote"], 
-        #     "link": resp_dict["result"][0]["link"]         
-        # }
-
-
-        #insert it into DB
-        #r_db = requests.post('http://localhost:3000/insertRecord', data=msg_payload_dic)
+            try:
+                r_db = requests.post('http://localhost:3000/insertRecord', data=msg_payload_dic)
+            except:
+                print "Connection Error on [DataBase]"
+                resp_dict = { "Error": "Connection Error on [DataBase]"}
 
         js = json.dumps(resp_dict)
         #print js
